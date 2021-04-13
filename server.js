90e984abaeb46c0fd5d71c2d23473d45d963c824
Susan Fujii <susan.fujii@me.com>
2021-04-12 20:13:50 -0500

    Implemented Add New Employee. Safe revert point.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 
+let roleId;
+let firstName;
+let lastName;
+
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
@@ -76,53 +80,137 @@ const viewEmployeesByDept = () => {
 
 
 
-// const viewEmployeesByManager = () => {
-//     connection.query(`SELECT
-//     m.id AS Manager_ID,
-//     CONCAT(m.first_name, ' ' ,m.last_name) AS Manager,
-//     CONCAT(e.first_name, ' ' ,e.last_name) AS Employee
-// FROM employee e
-// INNER JOIN employee m ON m.id = e.manager_id
-// WHERE m.id = 540
-// ORDER BY MANAGER;`, (error, result) => {
-//         if (error) throw error;
+const viewEmployeesByManager = () => {
+    connection.query(`SELECT id, first_name, last_name 
+    FROM employee 
+    WHERE (id IN (SELECT manager_id FROM employee));`, (error, result) => {
+        if (error) throw error;
 
-//         inquirer.prompt([
-//             {
-//                 message: "Which Manager Would You Like to See Direct Reports For?",
-//                 name: "whichManager",
-//                 type: "list",
-//                 choices: result.map((manager) => {
-//                     return {
-//                         name: `${manager.id}: ${manager.first_name} ${manager.last_name}`,
-//                         value: manager
-//                     }
-//                 })
-//             }
-//         ]).then(({ whichManager }) => {
-//             console.log("Which Manager" + whichManager);
+        inquirer.prompt([
+            {
+                message: "Which Manager Would You Like to See Direct Reports For?",
+                name: "whichManager",
+                type: "list",
+                choices: result.map((manager) => {
+                    console.log("Manager: ", manager);
+                    return {
+                        name: `${manager.id}: ${manager.first_name} ${manager.last_name}`,
+                        value: manager
+                    }
+                })
+            }
+        ]).then(({ whichManager }) => {
+            console.log("Which Manager.id" + whichManager.id);
 
-//             connection.query(`SELECT
-//             e.id,
-//             CONCAT(e.first_name, ' ' ,e.last_name) AS Employee,
-//             role.title AS Title,
-//             department.name AS Department,
-//             CONCAT("$",role.salary) AS Salary,
-//             IFNULL(CONCAT(m.first_name, ' ' ,m.last_name), 'NO MANAGER') AS Manager
-//         FROM employee e
-//         LEFT JOIN employee m ON e.manager_id = m.id
-//         INNER JOIN role ON e.role_id = role.id
-//         INNER JOIN department ON role.department_id = department.id
-//         WHERE role.department_id = ?
-//         ORDER BY e.id;`, whichDept.id, (error, results) => {
-//             if (error) throw error;
+            connection.query(`SELECT 
+                e.id,
+                CONCAT(e.first_name, ' ' ,e.last_name) AS Employee,
+                role.title AS Title,
+                department.name AS Department,
+                CONCAT("$",role.salary) AS Salary
+            FROM employee e
+            INNER JOIN role ON e.role_id = role.id
+            INNER JOIN department ON role.department_id = department.id
+            WHERE e.manager_id = ?
+            ORDER BY e.id;`, whichManager.id, (error, results) => {
+                if (error) throw error;
 
-//             console.table(results);
-//             initInquirer();
-//         })
-//         })
-//     })
-// };
+                console.log(`
+                ${whichManager.first_name} ${whichManager.last_name}'s Direct Reports: `);
+                console.table(results);
+                initInquirer();
+            })
+        })
+    })
+};   
+
+const setRoleId = (roleID) => {
+roleId = roleID;
+return roleId;
+}
+
+const setFirstName = (fName) => {
+    firstName = fName;
+    return firstName;
+}
+
+const setLastName = (lName) => {
+    lastName = lName;
+    return lastName;
+}
+
+const addNewEmployee = () => {
+// INSERT INTO employee (first_name, last_name, role_id, manager_id)
+// VALUES ("Susan", "Fujii", 29, 15);
+
+    connection.query(`SELECT 
+    role.id AS ID, 
+    role.title AS Title
+    FROM role;`, (error, result) => {
+    if (error) throw error;
+
+    inquirer.prompt([
+        {
+            message: "What is the employee's first name?",
+            type: "input",
+            name: "empFirstName"
+        },
+        {
+            message: "What is the employee's last name?",
+            type: "input",
+            name: "empLastName"
+        },
+        {
+            message: "What is the employee's role?",
+            type: "list",
+            name: "roleName",
+            choices: result.map((role) => {
+                console.log("Role: ", role);
+                return {
+                    name: `${role.ID}: ${role.Title}`,
+                    value: role
+                }
+            })
+        }]).then(({ empFirstName, empLastName, roleName }) => {
+                setRoleId(roleName.ID);
+                setFirstName(empFirstName);
+                setLastName(empLastName);
+        
+                connection.query(`SELECT id, first_name, last_name 
+                FROM employee 
+                WHERE (id IN (SELECT manager_id FROM employee));`, (error, result) => {
+                    if (error) throw error;
+        
+                    inquirer.prompt([
+                        {
+                        message: "What is the employee's new manager's name?",
+                        type: "list",
+                        name: "whichManager",
+                        choices: result.map((manager) => {
+                            console.log("Manager: ", manager.id, manager.first_name, manager.last_name);
+                            return {
+                                name: `${manager.id}: ${manager.first_name} ${manager.last_name}`,
+                                value: manager
+                                }
+                            })
+                        }
+                ]).then(({ whichManager }) => {
+                    console.log("Manager Id: " + whichManager.id);
+                    console.log("First Name: ", firstName);
+                    console.log("Last Name: ", lastName);
+                    console.log("Role Id: ", roleId);
+
+                    connection.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                        VALUES ("${firstName}", "${lastName}", ${roleId}, ${whichManager.id});`, (error, result) => {
+                            if (error) throw error;
+                            
+                        console.log("Successfully added new employee!");
+                        initInquirer();
+                    })
+                })
+            });
+    });
+})}
 
 const viewDepartments = () => {
     connection.query(`SELECT
@@ -188,7 +276,8 @@ const initInquirer = () => {
                 return viewEmployeesByDept(); 
             case "View All Employees By Manager":
                 return viewEmployeesByManager(); 
-            // "Add New Employee", 
+            case "Add New Employee":
+                return addNewEmployee(); 
             // "Remove Employee", 
             // "Update Employee Role", 
             // "Update Employee Manager", 
